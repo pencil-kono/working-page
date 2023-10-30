49995b012e5e21f66bcfa19de96b241f624ba1c5
kono <ykbubu4410@gmail.com>
2023-10-30 23:37:29 +0900

    一時開発完了

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const accItems = document.querySelectorAll('.p-faq-accordion__item');
-    const MAX_MOBILE_WIDTH = 1024;
+    const MAX_MOBILE_WIDTH = 768;
+    const MAX_TAB_WIDTH = 1024;
 
+    //アコーディオンの表示非表示の制御
     accItems.forEach(function(item) {
         item.addEventListener('click', function() {
             const accTtl = this.querySelector('.p-faq-accordion__ttl');
@@ -11,12 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    if (window.innerWidth <= MAX_MOBILE_WIDTH) {
+    if (window.innerWidth <= MAX_TAB_WIDTH) {
 
         const header = document.querySelector('.l-header');
         const navButton = document.querySelector(".nav-button");
         let lastScrollPosition = 0;
-        
+
+
+        //スクロール時の表示非表示の制御
         window.addEventListener('scroll', function() {
             let currentScrollPosition = window.scrollY;
             if (currentScrollPosition > lastScrollPosition) {
@@ -27,31 +31,27 @@ document.addEventListener("DOMContentLoaded", function() {
             lastScrollPosition = currentScrollPosition;
         });
 
+        //ハンバーガーメニューの制御
         navButton.addEventListener("click", e => {
             e.preventDefault();
-
             document.body.classList.toggle("nav-visible");
         });
+    }
 
 
+    //3行以上になったら末尾に...をつける
+    if (window.innerWidth <= MAX_MOBILE_WIDTH) {
         const textContainer = document.querySelectorAll('.p-reform-achieve__cont');
         textContainer.forEach(txt => {
             const lineHeight = parseInt(window.getComputedStyle(txt).lineHeight);
-            const maxHeight = lineHeight * 3; // 3行分の高さを計算
+            const maxHeight = lineHeight * 3;
             if (txt.scrollHeight > maxHeight) {
                 let originalText = txt.textContent.trim();
-
                 let truncatedText = originalText.slice(0, Math.floor(maxHeight / lineHeight) * originalText.length);
-                // 末尾の一文字を削除して...を追加
                 truncatedText = truncatedText.slice(0, -1) + '...';
                 txt.textContent = truncatedText;
-                // while (txt.scrollHeight > maxHeight) {
-                //     txt.textContent = txt.textContent.replace(/\W*\s(\S)*$/, '...'); // 最後の単語を削除し、...を追加
-                // }
             }
         });
-
-
     }
 
 });
